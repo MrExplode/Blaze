fb59e8f0061ef8ab54415adc580330d73f7c0745
MrExplode <me@sunstorm.rocks>
2022-11-11 20:55:58 +0100

    Relicense to MIT License

--- src/main/java/me/sunstorm/blaze/Animator.java
+++ src/main/java/me/sunstorm/blaze/Animator.java
@@ -1,21 +1,3 @@
-/*
- *     Blaze - Animation Library
- *     Copyright (C) 2021  SunStorm (aka. MrExplode)
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package me.sunstorm.blaze;
 
 import org.jetbrains.annotations.NotNull;
